9ceca9bc1eeffe18df1e4f45d458b1d489e41e0e
incredible7 <123460801+incredible7@users.noreply.github.com>
2025-02-02 16:54:49 +0100

    made hyper plane + point structs + distance method for brute force solution

--- exhaustive.go
+++ exhaustive.go
@@ -4,3 +4,35 @@ Set up currently to run on the Cifar-10 dataset only.
 
 package main
 
+import "fmt"
+import "math"
+
+func main() {
+	// dataset := ...
+	// Query:= ...
+
+}
+
+type Point struct {
+	ID          int
+	Coordinates []float64
+}
+
+type Hyperplane struct {
+	Q []float64
+	B float64
+}
+
+func (h *Hyperplane) dist(p *Point) float64 {
+	numerator := h.B
+	for i := range p.Coordinates {
+		numerator = numerator + h.Q[i]*p.Coordinates[i]
+	}
+	numerator = math.Abs(numerator)
+	denominator := 0.0
+	for i := range h.Q {
+		denominator = denominator + h.Q[i]*h.Q[i]
+	}
+	denominator = math.Sqrt(denominator)
+	return numerator / denominator
+}
