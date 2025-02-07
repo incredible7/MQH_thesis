03d65471bc3d68180f9de6b5f8594eaf8dc73f56
llbob <miniporse@gmail.com>
2025-02-07 14:20:15 +0100

    Add start of kmeans

--- pkg/algorithms/mqh/kmeans.go
+++ pkg/algorithms/mqh/kmeans.go
@@ -1,1 +1,13 @@
 package algorithms
+
+import {
+	"random"
+}
+
+func kmeans (data []Point, d int, k int) map[int][]float32 {
+	map := make(map[int][]float32)
+
+
+}
+
+func initialize(k int, d int, ) 
